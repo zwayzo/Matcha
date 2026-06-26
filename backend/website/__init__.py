e2f4b6eec393a1e4be02301e0950e76b9482c25d
zizo <elgrandesimo53@gmail.com>
2026-06-26 13:25:26 +0200

    still fixing error

--- backend/website/__init__.py
+++ backend/website/__init__.py
@@ -30,7 +30,7 @@ def create_app():
              }
          },
          allow_headers=["Content-Type", "Authorization", "Access-Control-Allow-Credentials"],
-         origins=["http://localhost:3000", "http://localhost:3001", "http://localhost:5001", "http://localhost:5001"],
+         origins=["http://localhost:3000", "http://localhost:3001", "http://localhost:5001", "http://localhost:5001", "https://matcha-gules.vercel.app"],
          supports_credentials=True
     )
     
